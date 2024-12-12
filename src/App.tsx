5e689445bc6d896ae8b02d50bb9a2dd18a49df31
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-12-12 08:14:09 -0300

    wip: toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 function App() {
-  return <></>;
+  return (
+    <div className="min-h-dvh w-screen grid place-content-center">
+      <label className="cursor-pointer">
+        <input type="checkbox" className="sr-only" />
+        <div className="flex w-24 h-12 bg-zinc-700 rounded-full px-1 items-center after:rounded-full after:content-[''] after:flex after:h-10 after:w-10 after:relative after:bg-zinc-800 "></div>
+      </label>
+    </div>
+  );
 }
 
 export default App;
