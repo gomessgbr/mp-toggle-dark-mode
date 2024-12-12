bd9685c3f2850757c1ee23fa4224957c5ff4a0f2
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-12-12 18:05:46 -0300

    feat: create animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,22 @@
+import { transform } from "typescript";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        moon: "moonGrow 0.7s linear 0.5s",
+      },
+      keyframes: {
+        moonGrow: {
+          "0%": { backgroundSize: "24px 24px" },
+          "50%": { backgroundSize: "20px 20px" },
+          "100%": { backgroundSize: "24px 24px" },
+        },
+      },
+    },
   },
   plugins: [],
-  darkMode: 'class'
-
+  darkMode: "class",
 };
