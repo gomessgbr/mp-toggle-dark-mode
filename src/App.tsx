4a490412915916053b07334278d70701f942b6e6
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-12-12 17:46:45 -0300

    fix: dark and light mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
+import { useEffect } from "react";
+
 function App() {
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, []);
+
+  const handleDarkMode = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const isChecked = event.target.checked;
+    if (isChecked) {
+      localStorage.setItem("theme", "light");
+      document.documentElement.classList.remove("dark");
+      return;
+    } else {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    }
+  };
   return (
     <div className="min-h-dvh w-screen grid place-content-center">
       <label className="cursor-pointer relative">
-        <input type="checkbox" className="sr-only peer" />
-        <div className="peer after:left-0 peer-checked:after:left-12 absolute flex w-24 h-12 bg-zinc-700 rounded-full px-1 items-center after:rounded-full after:content-[''] after:flex after:h-10 after:w-10 after:relative after:bg-zinc-800 "></div>
+        <input
+          type="checkbox"
+          className="sr-only peer"
+          onChange={handleDarkMode}
+        />
+        <div className="peer after:transition-all duration-500 after:left-0 peer-checked:after:left-12 absolute flex w-24 h-12 dark:bg-zinc-700 bg-zinc-300 rounded-full px-1 items-center after:rounded-full after:content-[''] after:flex after:h-10 after:w-10 after:relative dark:after:bg-zinc-800 after:bg-zinc-400 "></div>
       </label>
     </div>
   );
